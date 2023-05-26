afeda350f13784a5ed420f3c6cbb0c09d2998f8b
ruckel <kevin.dybeck@gmail.com>
2023-05-26 16:10:21 +0200

    basic spring boot security implemented

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -21,11 +21,13 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation ("org.springframework.boot:spring-boot-starter-security")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation ("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
