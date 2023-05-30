ebd28b8654ab64ff89db1a006f67848502e0d1af
Melinda Walter <melinda_walter@hotmail.com>
2023-05-30 14:26:07 +0200

    testing error handling

--- orders-service/src/main/java/com/example/ordersservice/controllers/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.ordersservice.controllers;
-
-public class ProductNotFoundException extends RuntimeException {
-
-    ProductNotFoundException(){
-        super("Product with id " + id +" could not be found.");
-    }
-}
